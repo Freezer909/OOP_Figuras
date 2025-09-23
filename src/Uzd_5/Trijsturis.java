7fdaf19ac830927ae24adccb46b5f275b166f482
Viktors Luks <luks.viktors@gmail.com>
2025-09-23 12:02:37 +0300

    pabeigta klase Trijsturis

---
+++ src/Uzd_5/Trijsturis.java
@@ -0,0 +1,29 @@
+package Uzd_5;
+
+public class Trijsturis {
+	public double mala1, mala2, mala3;
+	public Centrs punkts;
+	
+	public Trijsturis(Centrs p ,double m1, double m2, double m3) {
+		punkts = p;
+		mala1 = m1;
+		mala2 = m2;
+		mala3 = m3;
+	}
+	
+	public String izvaditTrijsturaInfo() {
+		return"Mala1: "+mala1+"cm\n"
+			 +"Mala2: "+mala2+"cm\n"
+			 +"Mala3: "+mala3+"cm\n";
+	}
+	
+	// uztaisit metodi laukuma apreiķinam
+	public double tlaukums() {
+		double S, p;
+		p = (mala1*mala2*mala3) /2;
+		S = Math.sqrt(p*(p-mala1)*(p-mala2)*(p-mala3));
+		
+		return Math.round(S*100.0)/100.0;
+	}
+	
+}

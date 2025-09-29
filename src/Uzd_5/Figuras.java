446d5b658c762b8bcb92fa4ff50112cdc1a1cbcc
Viktors Luks <luks.viktors@gmail.com>
2025-09-29 10:42:41 +0300

    Pabeigt OIzveide klase

--- src/Uzd_5/Figuras.java
+++ src/Uzd_5/Figuras.java
@@ -1,53 +1,69 @@
 package Uzd_5;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 import Uzd_1.MinkaTante;
 
 public class Figuras {
+	static ArrayList<Centrs> centraObjekti = new ArrayList<>();
+	static ArrayList<Cetrsturis> cetrsturaObjekti = new ArrayList<>();
+	static ArrayList<Aplis> aplaObjekti = new ArrayList<>();
+	static ArrayList<Trijsturis> trijsturaObjekti = new ArrayList<>();
+	
+	static String[] objektuPogas = {"Centrs", "Četrstūris", "Aplis", 
+			"Trijstūris", "Atgriezties"};
+	
 
 	public static void main(String[] args) {
-		/*//Izveido Centrs objektu
+		/*// izveido Centrs objektu
 		Centrs centraP1 = new Centrs(-10, 12);
-		System.out.println(centraP1.izvaditKordinatas());
+		System.out.println(centraP1.izvaditKoordinatas());
 		centraP1.parvietot(5, -2);
-		System.out.println(centraP1.izvaditKordinatas());
+		System.out.println(centraP1.izvaditKoordinatas());
 		
-		//Izveido Centrsturis objektu
+		// izveido Cetrsturis objektu
 		Cetrsturis cetrsturis1 = new Cetrsturis();
 		System.out.println(cetrsturis1.izvaditCetrsturaInfo());
-		//Izvadīt ši četrstūra atrašanās vietu
-		System.out.println(cetrsturis1.punkts.izvaditKordinatas());
-		
+		// izvadīt šī četrstūra atrašanās vietu
+		System.out.println(cetrsturis1.punkts.izvaditKoordinatas());
 		
 		Cetrsturis cetrsturis2 = new Cetrsturis(centraP1, 5, 11);
-		System.out.println(cetrsturis2.izvaditCetrsturaInfo()
-				+"\n"+cetrsturis2.punkts.izvaditKordinatas()
-				+"\nS = "+cetrsturis2.cLaukums());
+		System.out.println(cetrsturis2.izvaditCetrsturaInfo()+"\n"+
+				cetrsturis2.punkts.izvaditKoordinatas()+
+				"\ns = "+cetrsturis2.cLaukums());
 		
 		Centrs punkts2 = new Centrs (5, 23);
 		Aplis aplis1 = new Aplis(punkts2, 5);
-		System.out.println(aplis1.izvadiAplaInfo()+"\nS = "+
-		aplis1.rLaukums()+"\nC = "+aplis1.RLinijasGarums());*/
-		int cObjSk = 
-				MinkaTante.skaitlaParbaude("Cik centra objektus glabāsi", 1, 99);
-		if(cObjSk == -1)
-			cObjSk = 1;
-		Centrs[] centraObjekti = new Centrs[cObjSk];
+		System.out.println(aplis1.izvaditAplaInfo()+"\n"+
+		"s = "+aplis1.rLaukums()+"\n"+
+			"c = "+ aplis1.rLGarums());
+			*/
+		int izvele, poga;
 		
-		int ceObjSk = 
-				MinkaTante.skaitlaParbaude("Cik četrstūra objektus glabāsi", 1, 99);
-		if(ceObjSk == -1)
-			ceObjSk = 1;
-		Centrs[] centrsturaObjekti = new Centrs[ceObjSk];
-		int aObjSk = 
-				MinkaTante.skaitlaParbaude("Cik apļa objektus glabāsi", 1, 99);
-		if(aObjSk == -1)
-			aObjSk = 1;
-		Centrs[] aplaObjekti = new Centrs[aObjSk];
-		int tObjSk = 
-				MinkaTante.skaitlaParbaude("Cik trijstūra objektus glabāsi", 1, 99);
-		if(tObjSk == -1)
-			tObjSk = 1;
-		Centrs[] trijsturabjekti = new Centrs[tObjSk];
+		do {
+			izvele = MinkaTante.skaitlaParbaude(
+					"Ko vēlies darīt?\n"
+					+ "0 - Apturēt\n"
+					+ "1 - Izveidot objektu\n"
+					+ "2 - Aplūkot objektus\n"
+					+ "3 - Izsaukt metodi", 0, 3);
+			if (izvele == -1)
+				izvele = 0;
+			
+			switch (izvele) {
+			case 0:
+				JOptionPane.showMessageDialog(null,
+				"Programma apturēta!", "Paziņojums", 
+				JOptionPane.INFORMATION_MESSAGE);
+				break;
+				
+			case 1:
+				OIzveide.izveidotObjektu();
+				break;
+				
+			}
+		} while (izvele != 0);
 		
 	}
-
 }
--- src/Uzd_5/OIzveide.java
+++ src/Uzd_5/OIzveide.java
@@ -6,48 +6,99 @@ import Uzd_1.MinkaTante;
 
 public class OIzveide {
 	
+	static int cPIzvele() {
+		if (Figuras.centraObjekti.size() < 1)
+			return -1;
+		else
+		return Integer.parseInt(JOptionPane.showInputDialog(null, IzveidotieObjekti.izvadit(Figuras.centraObjekti)));
+	}
+	
 	static void izveidotObjektu() {
-	int x, y, cPnr;
-	Double p, a;
-	
-	int izvele = JOptionPane.showOptionDialog(null, "Kuru objektu veidot?", "Izvēle", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, Figuras.objektuPogas, Figuras.objektuPogas[0]);
-		if(izvele == -1)
+		int x, y, cPNr;
+		double p, a;
+		
+		int izvele = JOptionPane.showOptionDialog(null, 
+		"Kuru objektu veidot?", "Izvēle", 
+		JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
+		null, Figuras.objektuPogas, Figuras.objektuPogas[0]);
+		
+		if (izvele == -1)
 			izvele = 0;
-	
 		switch(izvele) {
 		case 0:
-			x = MinkaTante.skaitlaParbaude("Ievadi centra punkta x koordinātas", -100, 100);
-			y = MinkaTante.skaitlaParbaude("Ievadi centra punkta x koordinātas", -100, 100);
-			//izvidot centra punkta objektu un ieliktu masīvā
-			Figuras.centraObjekti.add(new Centrs(x,y));
+			x = MinkaTante.skaitlaParbaude(
+			"Ievadi centra punkta x koordinātas", -100, 100);
+			y = MinkaTante.skaitlaParbaude(
+			"Ievadi centra punkta y koordinātas", -100, 100);
+			// Izveidot centra punkta objektu un ielikt dinamiskajā masīvā
+			Figuras.centraObjekti.add(new Centrs(x, y));
 			break;
 			
 		case 1:
-			String[] ceIzvelesVeids = {"Noklusējuma", "Centra punkts un malas"};
+			String[] ceIzvelesVeids = 
+			{"Noklusējuma", "Centra punkts un malas"};
 			
-			String veids = (String) JOptionPane.showInputDialog(null, "Izvēlies, kuru konstruktoru izsaukt", "Četrstura izveide", JOptionPane.QUESTION_MESSAGE, null, ceIzvelesVeids, ceIzvelesVeids[0]);
+			String veids = (String)JOptionPane.showInputDialog(null, 
+			"Izvēlies, kuru konstruktoru izsaukt", 
+			"Četrstūra izveide", JOptionPane.QUESTION_MESSAGE, 
+			null, ceIzvelesVeids, ceIzvelesVeids[0]);
 			
 			if(veids == null)
 				veids = ceIzvelesVeids[0];
+				
 			switch (veids) {
 			case "Noklusējuma":
-				Figuras.CetrsturaObjekti.add(new Cetrsturis());
+				Figuras.cetrsturaObjekti.add(new Cetrsturis());
 				break;
 				
 			case "Centra punkts un malas":
-				cPnr = 0;
-				p = MinkaTante.skaitlaParbaude("Ievadi četrstūra platumu", 1, 100);
-				a = MinkaTante.skaitlaParbaude("Ievadi četrstūra augstumu", 1, 100);
-				Figuras.centraObjekti.add(new Cetrsturis(Figuras.centraObjekti.get(cPnr), p, a));
+				cPNr = cPIzvele();
+				if (cPNr == -1) {
+					JOptionPane.showMessageDialog(null, "Nav uztaisīts centra punkts!", "Kļūda?", JOptionPane.ERROR_MESSAGE);
+					break;
+				}
+				p = MinkaTante.skaitlaParbaude(
+						"Ievadi četrstūra platumu", 1, 100);
+				a = MinkaTante.skaitlaParbaude(
+						"Ievadi četrstūra augstumu", 1, 100);
+				Figuras.cetrsturaObjekti.add(new Cetrsturis(
+						Figuras.centraObjekti.get(cPNr), p, a));
 				break;
-				
 			}
 			break;
 		
-		case 4:
-			JOptionPane.showMessageDialog(null, "Atgriežas uz galveno izvēlni", "Info", JOptionPane.INFORMATION_MESSAGE);
+		case 2:
+			cPNr = cPIzvele();
+			if (cPNr == -1) {
+				JOptionPane.showMessageDialog(null, "Nav uztaisīts centra punkts!", "Kļūda?", JOptionPane.ERROR_MESSAGE);
+				break;
+			}
+			int r = MinkaTante.skaitlaParbaude("Ievadi apļa radiusu", 1, 100);
+			Figuras.aplaObjekti.add(new Aplis(Figuras.centraObjekti.get(cPNr), r));
 			break;
+			
+		case 3:
+			cPNr = cPIzvele();
+			if (cPNr == -1) {
+				JOptionPane.showMessageDialog(null, "Nav uztaisīts centra punkts!", "Kļūda?", JOptionPane.ERROR_MESSAGE);
+				break;
+			}
+			int m1 = MinkaTante.skaitlaParbaude(
+					"Ievadi trijstura pirmo malu", 1, 100);
+			int m2 =MinkaTante.skaitlaParbaude(
+					"Ievadi trijsturas otro malu", 1, 100);
+			int m3 = MinkaTante.skaitlaParbaude(
+					"Ievadi trijstura trešo malu", 1, 100);
+			if(m1 == -1 || m2 == -1 || m3 == -1)
+				break;
+					Figuras.trijsturaObjekti.add(new Trijsturis(Figuras.centraObjekti.get(cPNr), m1, m2, m3));
+			break;
+			
+		case 4:
+			JOptionPane.showMessageDialog(null,
+			"Atgriežas uz galveno izvēlni!", "Paziņojums", 
+			JOptionPane.INFORMATION_MESSAGE);
+			break;	
 		}
 	}
-	
 }
